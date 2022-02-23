c3cea27e901e419d9974d3293187bbd3d8517080
tarunfy <tarunsharma8920@gmail.com>
2022-02-23 23:09:38 +0530

    created add-details form

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,81 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
+import React from "react";
+import { FiCheck } from "react-icons/fi";
+import calendar30 from "../assets/images/calendar30.svg";
+import calendar31 from "../assets/images/calendar31.svg";
 
 const Details = () => {
-  const { currentUser } = useContext(AuthContext);
+  const handleSubmit = () => {};
 
-  console.log(currentUser);
-
-  return <div>Details</div>;
+  return (
+    <div className="h-screen bg-gradient-to-b bg-secondary-50 flex  justify-center items-center">
+      <h1 className="text-6xl font-bold text-black -rotate-90">
+        Create Your Business
+      </h1>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white w-128 shadow-custom2  p-4 text-black"
+      >
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="text-xl p-2 border-2 w-full focus:border-2 focus:border-primary-400 border-gray-300 outline-none"
+        />
+        <input
+          type="text"
+          placeholder="Business Name"
+          className="text-xl p-2 border-2 my-4 w-full focus:border-2 focus:border-primary-400 border-gray-300 outline-none"
+        />
+        <input
+          type="number"
+          placeholder="Staff Size"
+          className="text-xl p-2 border-2 w-full mb-1 focus:border-2 focus:border-primary-400 border-gray-300 outline-none"
+        />
+        <div className="my-4">
+          <label htmlFor="working-hours" className="text-secondary-300 text-lg">
+            How many hours does your staff work in a shift?
+          </label>
+          <input
+            type="time"
+            id="working-hours"
+            defaultValue="08:00"
+            placeholder="Staff Working Hours"
+            className="text-xl p-2 border-2 hover:border-primary-500 cursor-text focus:border-2 focus:border-primary-400 w-full border-gray-300 outline-none"
+          />
+        </div>
+        <div>
+          <label htmlFor="working-hours" className="text-secondary-300 text-lg">
+            How do you calculate monthly salary?
+          </label>
+          <div className="bg-secondary-50 cursor-pointer hover:outline-primary-400 hover:outline-1 outline outline-0 p-2 flex justify-between items-center">
+            <img src={calendar31} alt="calendar31" />
+            <div className="flex flex-col justify-center items-center">
+              <h3 className="text-sm font-semibold">Calendar Month</h3>
+              <p className="text-xs font-semibold">
+                Ex: March - 31 days, April - 30 days, etc.
+              </p>
+            </div>
+            <FiCheck className="h-7 w-7" />
+          </div>
+          <div className="bg-secondary-50 cursor-pointer hover:outline-primary-400 hover:outline-1 outline outline-0 p-2 mt-4 flex justify-between items-center">
+            <img src={calendar30} alt="calendar30" />
+            <div className="flex flex-col justify-center items-center">
+              <h3 className="text-sm font-semibold">Every Month 30 Days</h3>
+              <p className="text-xs font-semibold">
+                Ex: March - 30 days, April - 30 days, etc.
+              </p>
+            </div>
+            <FiCheck className="h-7 w-7" />
+          </div>
+        </div>
+        <button className="w-full bg-primary-500 text-white font-semibold text-xl py-2 px-4 my-4 hover:bg-primary-600 duration-500 transition-colors ease-in-out">
+          Create Business
+        </button>
+        <button className="w-full text-primary-500 bg-white border-2 border-primary-500 font-semibold text-xl py-2 px-4 hover:bg-primary-50  duration-500 transition-colors ease-in-outs">
+          Clear
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Details;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,75 +1,74 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       boxShadow: {
-        'custom': 'rgba(0, 0, 0, 0.24) 0px 3px 8px'
+        custom: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+        custom2:
+          "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;",
       },
-      height:{
-        '128': '32rem'
+      height: {
+        128: "32rem",
       },
       width: {
-        '128': '32rem'
+        128: "32rem",
       },
       colors: {
-          "primary": {
-            "50": "#f5f6ff",
-            "100": "#ebecff",
-            "200": "#cdd0fe",
-            "300": "#afb3fd",
-            "400": "#737bfc",
-            "500": "#3742fa",
-            "600": "#323be1",
-            "700": "#2932bc",
-            "800": "#212896",
-            "900": "#1b207b",
-            "1000": "#0B1121",
-            "1100":"#0C243A"
+        primary: {
+          50: "#f5f6ff",
+          100: "#ebecff",
+          200: "#cdd0fe",
+          300: "#afb3fd",
+          400: "#737bfc",
+          500: "#3742fa",
+          600: "#323be1",
+          700: "#2932bc",
+          800: "#212896",
+          900: "#1b207b",
+          1000: "#0B1121",
+          1100: "#0C243A",
         },
-          "secondary": {
-            "50": "#f4f5f6",
-            "100": "#eaeaed",
-            "200": "#cacbd2",
-            "300": "#aaabb7",
-            "400": "#6b6d80",
-            "500": "#2b2e4a",
-            "600": "#272943",
-            "700": "#202338",
-            "800": "#1a1c2c",
-            "900": "#151724"
+        secondary: {
+          50: "#f4f5f6",
+          100: "#eaeaed",
+          200: "#cacbd2",
+          300: "#aaabb7",
+          400: "#6b6d80",
+          500: "#2b2e4a",
+          600: "#272943",
+          700: "#202338",
+          800: "#1a1c2c",
+          900: "#151724",
         },
-          "tertiary": {
-            "50": "#feffff",
-            "100": "#fefefe",
-            "200": "#fcfdfd",
-            "300": "#fbfbfb",
-            "400": "#f7f8f9",
-            "500": "#f4f5f6",
-            "600": "#dcdddd",
-            "700": "#b7b8b9",
-            "800": "#929394",
-            "900": "#787879"
-            
-        }
+        tertiary: {
+          50: "#feffff",
+          100: "#fefefe",
+          200: "#fcfdfd",
+          300: "#fbfbfb",
+          400: "#f7f8f9",
+          500: "#f4f5f6",
+          600: "#dcdddd",
+          700: "#b7b8b9",
+          800: "#929394",
+          900: "#787879",
+        },
       },
       animation: {
-        'gradient-x':'gradient-x 6s ease-in-out infinite',
+        "gradient-x": "gradient-x 6s ease-in-out infinite",
       },
       keyframes: {
-        'gradient-x': {
-          '0%, 100%': {
-              'background-size':'200% 200%',
-              'background-position': 'left center'
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
           },
-          '50%': {
-              'background-size':'200% 200%',
-              'background-position': 'right center'
-          }
-      },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
+};
