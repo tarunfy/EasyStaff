8ec65da1ec9d91472d748842732c213e17978d5b
tarunfy <tarunsharma8920@gmail.com>
2022-02-26 04:22:03 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,17 +8,25 @@ const Home = () => {
       <div id="blurry-gradient"></div>
       <div id="blurry-gradient-2"></div>
       <div className="bg-slate-50 h-screen main flex flex-col justify-center items-center px-10">
-        <h1 className="text-8xl text-center z-30 font-bold">
-          Track Staff's Attendence <br /> & Payroll.
+        <h1 className="text-8xl text-center z-30 font-bold cursor-default">
+          Track Staff's{" "}
+          <span className="hover:text-quadtiary-500 inline-block transform hover:-translate-y-2 transition duration-200 ease-in-out">
+            Attendence
+          </span>{" "}
+          <br /> &{" "}
+          <span className=" hover:text-quadtiary-500 inline-block transform hover:-translate-y-2 transition duration-200 ease-in-out">
+            Payroll
+          </span>
+          .
         </h1>
-        <p className="text-center leading-10 text-2xl z-30 font-semibold text-gray-700 mt-10">
+        <p className="text-center cursor-default leading-10 text-2xl z-30 font-semibold text-gray-700 mt-10">
           EasyStaff is a free staff attendance and payroll management app that
           enables employers <br /> to easily manage their staff with automatic
           salary calculation, <br /> salary payment and more.
         </p>
         <Link
           to="/login"
-          className="uppercase z-30 hover:bg-quadtiary-500  hover:text-white hover:shadow-primary-1100 hover:scale-105 duration-300 ease-in-out hover:shadow-2xl transition-all mt-10 text-2xl border-2 border-quadtiary-500 text-quadtiary-500 px-6 py-3"
+          className="uppercase z-30 font-semibold hover:bg-quadtiary-500  hover:text-white hover:shadow-primary-1100 hover:scale-105 duration-300 ease-in-out hover:shadow-2xl transition-all mt-10 text-2xl border-2 border-quadtiary-500 text-quadtiary-500 px-6 py-3"
           id="cta-btn"
         >
           Start Tracking
