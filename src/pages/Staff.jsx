a8da5d2ca99c70642957df4302ed29ad7b9f7fe0
tarunfy <tarunsharma8920@gmail.com>
2022-03-07 19:30:07 +0530

    added update staff modal

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -9,15 +9,30 @@ import notfound from "../assets/images/404.svg";
 
 const Staff = () => {
   const [addStaffModal, setAddStaffModal] = useState(false);
-  const handleOpen = () => {
+  const [updateStaffModal, setUpdateStaffModal] = useState(false);
+
+  const openUpdateStaffModal = () => {
+    setUpdateStaffModal(true);
+  };
+  const closeUpdateStaffModal = () => {
+    setUpdateStaffModal(false);
+  };
+  const openAddStaffModal = () => {
     setAddStaffModal(true);
   };
-  const handleClose = () => {
+  const closeAddStaffModal = () => {
     setAddStaffModal(false);
   };
-  const handleSubmit = () => {
-    console.log("hi");
+  const addNewStaff = () => {
+    console.log("adding new staff...");
   };
+
+  const updateExistingStaff = () => {
+    console.log("update existing staff...");
+  };
+
+  const handleRemove = () => {};
+  const handleEdit = () => {};
   return (
     <>
       <Sidebar />
@@ -25,7 +40,7 @@ const Staff = () => {
         <div className="flex justify-between items-center w-full  mb-10">
           <h1 className="text-6xl font-black">Your Staff</h1>
           <button
-            onClick={handleOpen}
+            onClick={openAddStaffModal}
             className=" uppercase font-semibold hover:bg-quadtiary-500  hover:text-white hover:shadow-primary-1100 hover:scale-105 duration-300 ease-in-out hover:shadow-2xl transition-all  text-xl border-2 border-quadtiary-500 text-quadtiary-500 px-6 py-2"
           >
             Add
@@ -47,7 +62,10 @@ const Staff = () => {
             </div>
             <div className="flex justify-end items-center space-x-4">
               <Tippy interactive={true} animation="scale" content="Edit staff">
-                <div className="p-2 border-[1px] hover:cursor-pointer border-zinc-800 hover:text-teal-500 hover:border-[1px] hover:border-teal-500 transition-all duration-300 ease-in-out">
+                <div
+                  onClick={openUpdateStaffModal}
+                  className="p-2 border-[1px] hover:cursor-pointer border-zinc-800 hover:text-teal-500 hover:border-[1px] hover:border-teal-500 transition-all duration-300 ease-in-out"
+                >
                   <MdEdit className="h-6 w-6" />
                 </div>
               </Tippy>
@@ -56,7 +74,10 @@ const Staff = () => {
                 animation="scale"
                 content="Remove staff"
               >
-                <div className="p-2 border-[1px] hover:cursor-pointer border-zinc-800 hover:text-red-500 hover:border-[1px] hover:border-red-500 transition-all duration-300 ease-in-out">
+                <div
+                  onClick={handleRemove}
+                  className="p-2 border-[1px] hover:cursor-pointer border-zinc-800 hover:text-red-500 hover:border-[1px] hover:border-red-500 transition-all duration-300 ease-in-out"
+                >
                   <MdPersonRemoveAlt1 className="h-6 w-6" />
                 </div>
               </Tippy>
@@ -70,28 +91,30 @@ const Staff = () => {
         </div> */}
       </div>
       <Modal
-        open={addStaffModal}
-        onClose={handleClose}
+        open={addStaffModal || updateStaffModal}
+        onClose={addStaffModal ? closeAddStaffModal : closeUpdateStaffModal}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
         className="flex h-screen w-full items-center justify-center"
       >
         <Box className="p-3 bg-slate-50 text-center border-none outline-none focus:outline-none ">
-          <h1 className="text-[2rem] font-semibold">Add Staff</h1>
+          <h1 className="text-[2rem] font-semibold">
+            {addStaffModal ? "Add Staff" : "Update Staff"}
+          </h1>
           <hr className="bg-slate-400 h-[2px] w-full mb-4" />
           <form
-            onSubmit={handleSubmit}
+            onSubmit={addStaffModal ? addNewStaff : updateExistingStaff}
             className="flex flex-col justify-start items-start space-y-3"
           >
             <div className="flex justify-between items-center w-full">
               <input
                 type="text"
-                placeholder="Full Name"
+                placeholder={addStaffModal ? "Full Name" : "Jhon Doe"}
                 className="w-[47%] p-2 text-lg focus:outline-quadtiary-400 border-2 border-gray"
               />
               <input
                 type="email"
-                placeholder="Email"
+                placeholder={addStaffModal ? "Email" : "jhondoe@gmail.com"}
                 className="w-[47%] p-2 text-lg focus:outline-quadtiary-400 border-2 border-gray"
               />
             </div>
@@ -101,44 +124,42 @@ const Staff = () => {
                 maxLength="10"
                 required
                 pattern="[0-9]{6}"
-                placeholder="Phone Number"
+                placeholder={addStaffModal ? "Phone Number" : "9827453621"}
                 className="w-[47%] p-2 text-lg focus:outline-quadtiary-400 border-2 border-gray"
               />
               <input
                 type="text"
-                placeholder="Designation"
+                placeholder={
+                  addStaffModal ? "Designation" : "Chief Technology Officer"
+                }
                 className="w-[47%] p-2 text-lg focus:outline-quadtiary-400 border-2 border-gray"
               />
             </div>
             <div className="flex justify-between items-center w-full">
               <input
                 type="text"
-                placeholder="Manager Name"
+                placeholder={addStaffModal ? "Manager Name" : "Om Prakash"}
                 className="w-[47%] p-2 text-lg focus:outline-quadtiary-500 border-2 border-gray"
               />
               <input
                 type="text"
-                placeholder="Salary"
+                placeholder={addStaffModal ? "Salary" : "25000"}
                 className="w-[47%] p-2 text-lg focus:outline-quadtiary-500 border-2 border-gray"
               />
             </div>
             <div className="flex justify-between items-center w-full">
-              <input
-                type="text"
-                placeholder="Address"
-                className="w-full p-2 text-lg focus:outline-quadtiary-500 border-2 border-gray"
-              />
-            </div>
-            <div className="flex justify-between items-center w-full">
-              <input
-                type="date"
-                placeholder="Address"
+              <textarea
+                placeholder={
+                  addStaffModal ? "Address" : "Street 48, Noida Sectore 48, UP."
+                }
                 className="w-full p-2 text-lg focus:outline-quadtiary-500 border-2 border-gray"
               />
             </div>
             <div className="flex justify-end items-center space-x-3">
               <button
-                onClick={handleClose}
+                onClick={
+                  addStaffModal ? closeAddStaffModal : closeUpdateStaffModal
+                }
                 className="font-semibold text-red-500 border-red-500 border-[1px]  duration-300 ease-in-out hover:shadow-xl transition-all  text-xl   px-6 py-2"
               >
                 Cancel
@@ -147,7 +168,7 @@ const Staff = () => {
                 type="submit"
                 className="font-semibold text-white bg-quadtiary-500   duration-300 ease-in-out hover:shadow-xl transition-all  text-xl  px-6 py-2"
               >
-                Add
+                Submit
               </button>
             </div>
           </form>
