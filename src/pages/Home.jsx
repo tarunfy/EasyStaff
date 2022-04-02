29ea474d7944a5a0abffce0d99e9ac94edf57b23
tarunfy <tarunsharma8920@gmail.com>
2022-04-02 23:31:11 +0530

    changes in ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import arrow from "../assets/images/arrow.svg";
+import { IoArrowForwardCircle } from "react-icons/io5";
 
 const Home = () => {
   return (
@@ -8,33 +9,36 @@ const Home = () => {
       <div id="blurry-gradient"></div>
       <div id="blurry-gradient-2"></div>
       <div className="bg-slate-50 h-screen main flex flex-col justify-center items-center px-10">
-        <h1 className="text-8xl font-sans text-center z-30 font-bold cursor-default">
-          Track Staff's{" "}
+        <h1
+          id="hero-heading"
+          className="text-8xl  font-sans text-center tracking-tighter z-30 font-bold cursor-default"
+        >
+          The{" "}
           <span className="hoverEffect hover:text-quadtiary-500 inline-block transform hover:-translate-y-2 transition duration-200 ease-in-out">
-            Attendence
+            Proffesional
           </span>{" "}
-          <br /> &{" "}
+          Staff{" "}
           <span className="hoverEffect hover:text-quadtiary-500 inline-block transform hover:-translate-y-2 transition duration-200 ease-in-out">
-            Payroll
-          </span>
-          .
+            Management
+          </span>{" "}
+          App you were waiting for.
         </h1>
         <p className="text-center cursor-default leading-8 text-xl z-30 font-light text-gray-700 mt-10">
           EasyStaff is a free staff attendance and payroll management app that
           enables employers <br /> to easily manage their staff with automatic
-          salary calculation, <br /> salary payment and more.
+          salary calculation, salary payment and more.
         </p>
         <Link
           to="/signin"
-          className="uppercase z-30 relative font-semibold hover:bg-quadtiary-500  hover:text-white hover:shadow-primary-1100 hover:scale-105 duration-300 ease-in-out hover:shadow-2xl transition-all mt-10 text-2xl border-2 border-quadtiary-500 text-quadtiary-500 px-6 py-3"
+          className="z-30 relative font-semibold hover:bg-quadtiary-500 flex items-center justify-between  hover:text-white hover:shadow-primary-1100 hover:scale-105 duration-300 ease-in-out hover:shadow-2xl transition-all mt-10 text-2xl border-2 border-quadtiary-500 text-quadtiary-500 px-6 py-3"
           id="cta-btn"
         >
-          Start Tracking
+          Start Tracking <IoArrowForwardCircle className="h-8 w-8 ml-1" />
         </Link>
         <img
           src={arrow}
           alt="scribble-arrow"
-          className="absolute bottom-16  z-0 left-[47%] h-20 w-16 slide-top"
+          className="absolute bottom-14  z-0 left-[47%] h-20 w-16 slide-top"
         />
       </div>
     </>
